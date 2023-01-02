c85671653fc1b39b53183cc69d96f0f43d61993c
karim.hemina <karimhemina16@gmail.com>
2023-01-02 20:05:28 +0100

    separate into components

---
+++ resources/views/components/listing-card.blade.php
@@ -0,0 +1,16 @@
+@props(['listing'])
+<x-card>
+    <div class="flex">
+        <img
+            class="hidden w-48 mr-6 md:block"
+            src="{{asset('images/no-image.png')}}"
+            alt=""
+        />
+        <div>
+            <h3 class="text-2xl">
+                <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
+            </h3>
+            <div class="text-xl font-bold mb-4">{{$listing->manufacturer}}</div>
+        </div>
+    </div>
+</x-card>
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -7,7 +7,7 @@
     ><i class="fa-solid fa-arrow-left"></i> Back
     </a>
     <div class="mx-4">
-        <div class="bg-gray-50 border border-gray-200 p-10 rounded">
+        <x-card class="p-10">
             <div
                 class="flex flex-col items-center justify-center text-center"
             >
@@ -19,29 +19,29 @@
 
                 <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
                 <div class="text-xl font-bold mb-4">{{$listing->manufacturer}}</div>
-{{--                <ul class="flex">--}}
-{{--                    <li--}}
-{{--                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"--}}
-{{--                    >--}}
-{{--                        <a href="#">Laravel</a>--}}
-{{--                    </li>--}}
-{{--                    <li--}}
-{{--                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"--}}
-{{--                    >--}}
-{{--                        <a href="#">API</a>--}}
-{{--                    </li>--}}
-{{--                    <li--}}
-{{--                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"--}}
-{{--                    >--}}
-{{--                        <a href="#">Backend</a>--}}
-{{--                    </li>--}}
-{{--                    <li--}}
-{{--                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"--}}
-{{--                    >--}}
-{{--                        <a href="#">Vue</a>--}}
-{{--                    </li>--}}
-{{--                </ul>--}}
-                <div class="border border-gray-200 w-full mb-6"></div>
+                <ul class="flex">
+                    <li
+                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
+                    >
+                        <a href="#">Laravel</a>
+                    </li>
+                    <li
+                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
+                    >
+                        <a href="#">API</a>
+                    </li>
+                    <li
+                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
+                    >
+                        <a href="#">Backend</a>
+                    </li>
+                    <li
+                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
+                    >
+                        <a href="#">Vue</a>
+                    </li>
+                </ul>
+                <div class="border border-gray-200 w-full m-4"></div>
                 <div>
                     <h3 class="text-3xl font-bold mb-4">
                         Item Description
@@ -59,6 +59,6 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </x-card>
     </div>
 @endsection
