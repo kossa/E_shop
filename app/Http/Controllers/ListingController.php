29170354dac30044241296fcf7ed24f13acbd1c2
karim.hemina <karimhemina16@gmail.com>
2023-01-03 16:35:43 +0100

    Filter the listings by tags

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,7 +9,7 @@ class ListingController extends Controller
 {
     public  function index(){
         return view('listings.index', [
-            'listings' => Listing::all()
+            'listings' => Listing::latest()->filter(request(['tag']))->get()
         ]);
     }
 
