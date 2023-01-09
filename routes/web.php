21356ff4f4e75a1137d62ff250abaf61e83e2625
Kouceyla <hadjikouceyla@gmail.com>
2023-01-09 12:57:12 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,36 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Auth routes
+Auth::routes();
+
 // All listings
 Route::get('/', [ListingController::class, 'index']);
 
-// Show create form
-Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
-
-// Store listing data
-Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
-
-// Show edit form
-Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
-
-// Update listing
-Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
-
-// Delete listing
-Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
-
 Route::get('/listings/manage', [ListingController::class, 'manage'])->middleware('auth');
 
-// Single listing
-Route::get('/listings/{listing}', [ListingController::class, 'show']);
+Route::resource('listings', ListingController::class)->middleware('auth');
 
-
-Route::get('/register', [UserController::class, 'create'])->middleware('guest');
-
-Route::post('/users', [UserController::class, 'store']);
-
-Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
-
-Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
-
-Route::post('/users/authenticate', [UserController::class, 'authenticate']);
